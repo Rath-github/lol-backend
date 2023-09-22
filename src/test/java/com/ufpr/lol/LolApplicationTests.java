63632ce500af3c21cf8cf1253fb06534aa4d8301
IamThiago-IT <thiagodossantos315@gmail.com>
2023-09-22 19:36:17 -0300

    Config

---
+++ src/test/java/com/ufpr/lol/LolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpr.lol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
