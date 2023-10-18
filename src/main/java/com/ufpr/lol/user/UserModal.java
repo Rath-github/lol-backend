9b6181b666965fda20f22102b4c5301bfe68f4cd
Thiago dos Santos <thiagodossantos315@gmail.com>
2023-10-17 22:35:16 -0300

    create entity and bank credentials

--- src/main/java/com/ufpr/lol/user/UserModal.java
+++ src/main/java/com/ufpr/lol/user/UserModal.java
@@ -1,4 +1,21 @@
 package com.ufpr.lol.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Data
 public class UserModal {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String cpf;
+    private String nome;
+    private String email;
+    private String endereco;
+    private String telefone;
 }
