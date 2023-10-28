6dec65bf73547f5d46a39f97db98d86e0227d73b
IamThiago-IT <thiagodossantos315@gmail.com>
2023-10-28 18:03:10 -0300

    Criação de ClienteModal

---
+++ src/main/java/com/ufpr/lol/Modal/ClienteModal.java
@@ -0,0 +1,22 @@
+package com.ufpr.lol.Modal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity(name = "tb_clientes")
+@Data
+public class ClienteModal {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    private UUID id;
+    private String cpf;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+    private String endereco;
+    private String telefone;
+    private String senha;
+}
