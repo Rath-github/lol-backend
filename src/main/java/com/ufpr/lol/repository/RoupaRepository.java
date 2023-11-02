b78a39f55ccb3de4cd33cfd69f2b7c30e2672129
IamThiago-IT <thiagodossantos315@gmail.com>
2023-11-02 20:51:54 -0300

    Update

--- src/main/java/com/ufpr/lol/repository/RoupaRepository.java
+++ src/main/java/com/ufpr/lol/repository/RoupaRepository.java
@@ -4,12 +4,15 @@ import com.ufpr.lol.modal.RoupaModal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RoupaRepository extends JpaRepository<RoupaModal, Long> {
     Optional<RoupaModal> findById(UUID id);
+
+    public List<RoupaModal> selectAll();
 
     void deleteById(UUID id);
 }
