09479a5138ad28d5ea65a38af63407024706774e
IamThiago-IT <thiagodossantos315@gmail.com>
2023-11-02 21:51:14 -0300

    Update

--- src/main/java/com/ufpr/lol/repository/FuncionarioRepository.java
+++ src/main/java/com/ufpr/lol/repository/FuncionarioRepository.java
@@ -4,14 +4,14 @@ import com.ufpr.lol.modal.FuncionarioModal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<FuncionarioModal, Long> {
     FuncionarioModal findByEmail(String email);
 
+    FuncionarioModal findById(UUID id);
     void deleteById(UUID id);
 
-    FuncionarioModal findById(UUID id);
+
 }
